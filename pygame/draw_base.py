efbc05602d18291caf46d4c599b1a9f66aa7b29d
Anna <kaczoro.anna@gmail.com>
2025-03-20 19:49:54 +0100

    Update draw_base.py
    with captitalize

--- pygame/draw_base.py
+++ pygame/draw_base.py
@@ -1,15 +1,15 @@
 import pygame
 
-RED = (255, 0, 0)  # rouge (RGB)
-BLUE = (0, 0, 255)  # bleu (RGB)
+RED = (255, 0, 0)  # Rouge (RGB)
+BLUE = (0, 0, 255)  # Bleu (RGB)
 
 pygame.init()
-clock = pygame.time.Clock()  # pour pouvoir contrôler la vitesse de rafrachissement
-screen = pygame.display.set_mode((800, 600))  # fenêtre graphique de 800x600 pixels
+clock = pygame.time.Clock()  # Pour pouvoir contrôler la vitesse de rafrachissement
+screen = pygame.display.set_mode((800, 600))  # Fenêtre graphique de 800x600 pixels
 
-screen.fill(BLUE)  # remplir l'écran en bleu
-pygame.draw.circle(screen, RED, center=(100, 200), radius=25)  # dessine un cercle rouge
-pygame.display.flip()  # applique les changements graphiques effectués sur l'écran
+screen.fill(BLUE)  # Remplit l'écran en bleu
+pygame.draw.circle(screen, RED, center=(100, 200), radius=25)  # Dessine un cercle rouge
+pygame.display.flip()  # Applique les changements graphiques effectués sur l'écran
 
 while True:
     clock.tick(10)  # 10 FPS (10 itérations par seconde)
