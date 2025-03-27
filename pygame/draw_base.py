8f484da8c42a092f28114355451c43a090a71a99
Anna <kaczoro.anna@gmail.com>
2025-03-27 21:55:52 +0100

    Fill in white

--- pygame/draw_base.py
+++ pygame/draw_base.py
@@ -1,5 +1,6 @@
 import pygame
 
+WHITE = (0, 0, 0)  # Blanc (RGB)
 RED = (255, 0, 0)  # Rouge Pur (RGB)
 YELLOW = (239, 216, 91) # Jaune pastel (RGB)
 
@@ -7,7 +8,7 @@ pygame.init()
 clock = pygame.time.Clock()  # Pour pouvoir contrôler la vitesse de rafraîchissement
 screen = pygame.display.set_mode((800, 600))  # Fenêtre graphique de 800x600 pixels
 
-screen.fill(BLUE)  # Remplit l'écran en bleu
+screen.fill(WHITE)  # Remplit l'écran en blanc
 pygame.draw.circle(screen, color=YELLOW, center=(100, 200), radius=25)  # Dessine un cercle jaune
 pygame.display.flip()  # Applique les changements graphiques effectués sur l'écran
 
