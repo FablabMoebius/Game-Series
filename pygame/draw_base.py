e81f6ec9d7e065bfafae2c666ad50fe404ca883a
Anna <kaczoro.anna@gmail.com>
2025-03-27 21:52:50 +0100

    Change circle color to yellow

--- pygame/draw_base.py
+++ pygame/draw_base.py
@@ -1,14 +1,14 @@
 import pygame
 
-RED = (255, 0, 0)  # Rouge (RGB)
-BLUE = (0, 0, 255)  # Bleu (RGB)
+RED = (255, 0, 0)  # Rouge Pur (RGB)
+YELLOW = (239, 216, 91) # Jaune pastel (RGB)
 
 pygame.init()
 clock = pygame.time.Clock()  # Pour pouvoir contrôler la vitesse de rafraîchissement
 screen = pygame.display.set_mode((800, 600))  # Fenêtre graphique de 800x600 pixels
 
 screen.fill(BLUE)  # Remplit l'écran en bleu
-pygame.draw.circle(screen, color=RED, center=(100, 200), radius=25)  # Dessine un cercle rouge
+pygame.draw.circle(screen, color=YELLOW, center=(100, 200), radius=25)  # Dessine un cercle jaune
 pygame.display.flip()  # Applique les changements graphiques effectués sur l'écran
 
 while True:
