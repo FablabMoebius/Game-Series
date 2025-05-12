31bceb69474c667d2d3d4c3146806bb6daf22a13
Anna <kaczoro.anna@gmail.com>
2025-05-12 20:42:19 +0200

    Update pong_base.py

--- pygame/pong_base.py
+++ pygame/pong_base.py
@@ -1,29 +1,27 @@
 import pygame
 
-WIDTH = 800
-HEIGHT = 600
-
-# ----- initialisation générale
+# Initialisation générale
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  # fenêtre graphique de 800x600 pixels
+screen = pygame.display.set_mode((800, 600))  # fenêtre graphique de 800x600 pixels
 clock = pygame.time.Clock()  # pour contrôler la vitesse de rafraîchissement
 
-# ----- initialisation des éléments du jeu (création de surfaces, etc.)
+# Initialisation des éléments du jeu (création de surfaces, etc.)
 
-# ----- boucle principale
+# Boucle principale
 going = True
 while going:
 
     clock.tick(60)  # 60 FPS
 
-    # ----- gestion des événements (souris, clavier, etc.)
+    # Gestion des événements (souris, clavier, etc.)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             going = False
 
-    screen.fill((0, 0, 0))
+    # Opérations à effectuer à chaque frame
+    screen.fill(color="black")
 
-    # ----- applique tous les changements effectués à l'écran
+    # Application des changements effectués à l'écran
     pygame.display.flip()
 
 pygame.quit()
